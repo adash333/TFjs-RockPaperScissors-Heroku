45a8d1693c58102f4a10a13a0805d43e0b166733
adash333 <adash333@hotmail.com>
2020-11-20 22:32:31 +0900

    modify server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 let express = require("express");
 let app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static("./static"));
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(app.get('port'), function(){
     console.log("Serving at 8080")
 });
